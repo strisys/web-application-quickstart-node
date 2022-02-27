7e0ce216c7c1c313bce810cf0ae5dc367839c053
Stephen Trudel <developer@strisys.com>
2022-02-27 18:17:22 -0500

    reorg - stage 2

---
+++ src/ui/src/ts/index.ts
@@ -0,0 +1,10 @@
+
+// import { CustomerRepository, Customer } from 'model-client';
+
+// var repository = new CustomerRepository();
+
+// repository.get().then((entities: Customer[]) => {
+//   console.log(entities)
+// })
+
+console.log('loaded');
