83a5b9b579e11361e240c3d0240d46203a975eba
Stephen Trudel <developer@strisys.com>
2022-02-27 09:00:49 -0500

    reorg - phase 2

---
+++ src/server/src/ts/test/customer-repository.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+describe('CustomerRepository', () => {
+  describe('sample test', function() {
+    it('should pass', async () => {
+      // Assemble/Arrange
+      const val = true;
+      
+      // Assert
+      expect(val).to.be.true;
+    });
+  });
+});
